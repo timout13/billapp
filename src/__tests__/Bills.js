0d7051ec94c30ee9cc798dd8b55f59ecfd0d677b
Timothe Lebrun <timothe463@icloud.com>
2024-03-08 12:16:11 +0100

    fix-unit: Then bill icon in vertical layout should be highlighted

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -28,7 +28,7 @@ describe("Given I am connected as an employee", () => {
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
-
+      expect(windowIcon.classList.contains('active-icon')).toBe(true);
     })
     test("Then bills should be ordered from earliest to latest", async () => {
       document.body.innerHTML = BillsUI({ data: bills })
